41259c84071529bdc05fc281b011b47845ad87c2
Cameron Boy <boyxx001@gmail.com>
2017-12-17 09:35:29 -0700

    Updated server list to run off google sheets

--- pings.py
+++ pings.py
@@ -1,25 +1,31 @@
+
 import requests
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
-with open('servers.json') as f:
-    sites = json.load(f)
+def getCredentials(path_to_Credentials):
+    scopes = ['https://spreadsheets.google.com/feeds']
+    return ServiceAccountCredentials.from_json_keyfile_name(path_to_Credentials, scopes)
 
-scope = ['https://spreadsheets.google.com/feeds']
+def main():
+    # Retrieve the appropriate credentials object
+    credentials = getCredentials('Gcred.json')
+    # Authorize those credentials
+    file = gspread.authorize(credentials)
+    # Retrieve the sheets required
+    sheet = file.open("Ping Monitoring").sheet1
+    servers = file.open("Sites").sheet1.get_all_records()
+    # Go through the servers and check their response time and write it to Ping Monitoring sheet
+    for idx in range(len(servers)):
+        url = servers[idx]["site"]
+        provider = servers[idx]["provider"]
+        typ = servers[idx]["type"]
+        response = requests.post(url).elapsed.total_seconds()
+        time = str(datetime.now())
+        row = [site, typ, provider, url, time, response]
+        sheet.append_row([site, typ, provider, url, time, response])
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name('GCred.json', scope)
-
-file = gspread.authorize(credentials)
-
-sheet = file.open("Ping Monitoring").sheet1
-
-for site in sites.keys():
-    url = sites[site]["url"]
-    provider = sites[site]["provider"]
-    typ = sites[site]["type"]
-    response = requests.post(url).elapsed.total_seconds()
-    time = str(datetime.now())
-    row = [site, typ, provider, url, time, response]
-    sheet.append_row([site, typ, provider, url, time, response])
+if __name__ == '__main__':
+    main()
